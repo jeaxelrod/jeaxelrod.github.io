d242ebad118cc4f7a1eb1a2df21ef9914408bd58
Jeremy Axelrod <axelrod.jeremy@gmail.com>
2014-06-17 10:24:37 -0700

    Change order of when header.js loads; Change some cotent of projects

--- public/js/header.js
+++ public/js/header.js
@@ -1,6 +1,4 @@
-window.onload = function() {
-  var pathname = document.URL.match(/[^\/]+/g)[2];
-  var active_link = document.getElementById(pathname);
-  active_link.className = "active";
-  active_link.href = "/"
-}
+var pathname = document.URL.match(/[^\/]+/g)[2];
+var active_link = document.getElementById(pathname);
+active_link.className = "active";
+active_link.href = "/"
